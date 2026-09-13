bdf429ce8fc2f05152c73d70e3adcc5fce874281
indium114 <156162907+indium114@users.noreply.github.com>
2026-09-13 15:37:05 +0200

    feat: db_path()

--- zdir-librarian/src/db.rs
+++ zdir-librarian/src/db.rs
@@ -1,3 +1,16 @@
+use redb::{
+  Database,
+  Error,
+  ReadableDatabase,
+  ReadableTable,
+  TableDefinition,
+};
+
+fn db_path() -> String {
+    "/tmp/zdir-".to_string() + &unsafe { libc::getuid().to_string() } + ".sock"
+}
+
 pub fn database() {
     println!("Database!");
+    println!("Path: {}", db_path());
 }
